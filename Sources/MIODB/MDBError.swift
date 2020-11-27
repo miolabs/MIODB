279c7542b59212f79b7d6cd3bb2d99e3cf0d406f
David Trallero <ditiem@gmail.com>
2020-11-27 17:06:03 +0100

    Added Db connection pool

---
+++ Sources/MIODB/MDBError.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by David Trallero on 26/11/2020.
+//
+
+import Foundation
+
+
+public enum MDBError: Error
+{
+    case cantBeginTransactionWhileInsideTransaction
+    case cantEndTransactionWhileNotInsideTransaction
+    case invalidPoolID( _ poolID: String, functionName: String = #function)
+    case createNotImplemented( functionName: String = #function )
+}
+
+
+extension MDBError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+            case .cantBeginTransactionWhileInsideTransaction:
+                return "[MIODBError] Can't begin transaction, another transaction is already in progress."
+            case .cantEndTransactionWhileNotInsideTransaction:
+                return "[MIODBError] Can't end transaction if a transaction is not in progress."
+            case let .invalidPoolID(poolID, functionName):
+                return "[MDBError] \(poolID) does not exists in MDBManager. Called from \"\(functionName)\"."
+            case let .createNotImplemented(functionName):
+                return "[MDBError] create function not implemented in MDBConnection. Called from \"\(functionName)\"."
+        }
+    }
+}

bf71b95ecf79bfb773ccbe6362279d33295bc5f3
Javier Segura <javier@miolabs.com>
2024-04-26 10:55:05 +0200

    updating branch

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/miolabs/MIOCore.git", .branch("master") ),
+        .package(url: "https://github.com/miolabs/MIOCore.git", .branch("context") ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
